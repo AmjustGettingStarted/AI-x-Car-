5fcc77c13ee1e1cd72e487588aa36dc4369e291c
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-12 15:45:57 +0530

    Removed the signup and signin commands s

--- components/header.jsx
+++ components/header.jsx
@@ -10,13 +10,7 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+  
     </div>
   );
 };
