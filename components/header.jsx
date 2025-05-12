0e56e0f7ee49bd5f2240258475f7ee32897207fc
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-12 16:28:44 +0530

    added car logo

--- components/header.jsx
+++ components/header.jsx
@@ -5,13 +5,19 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
-
+import logo from "@/public/logo.png";
 const Header = () => {
   return (
-    <div>
-  
-    </div>
+    <header>
+      <nav>
+        <Link href={"/"}>
+          <Image src={logo} alt="logo" width={50} height={40} />
+        </Link>
+      </nav>
+    </header>
   );
 };
 
