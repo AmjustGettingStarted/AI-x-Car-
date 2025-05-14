9269ae45ba7ab4ca73763bf3b9100f508eaa42c4
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-14 14:39:26 +0530

    Added the dotted background, initial step to the landing page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,18 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Hello world!</div>;
+  return (
+    <div className="pt-20 flex flex-col">
+      {/* Header */}
+      <section className="dotted-background py-16 md:py-28 ">
+        <div>
+          <div>Find Your Dream Car with AIxCAR</div>
+          <p>
+            Advanced AI Car Search and test drive from thousands of vehicles
+          </p>
+        </div>
+        {/* Search */}
+      </section>
+    </div>
+  );
 }
