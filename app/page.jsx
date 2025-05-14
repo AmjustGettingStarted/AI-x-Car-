d9f110177f42ca90f85462b29917bf36c3e07346
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-14 17:25:36 +0530

    All issues fixed, Fix: removed the @layer from global css and manually added each class to the title in the page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
       <section className="relative dotted-background py-16 md:py-28 ">
         <div className="max-w-4xl mx-auto text-center">
           <div className="mb-8">
-            <h1 className="gradient-title text-5xl md:text-8xl mb-4 text-white ">
+            <h1 className="text-5xl md:text-8xl mb-4 gradient font-extrabold tracking-tighter pr-2 pb-2 text-transparent bg-clip-text bg-gradient-to-br from-blue-500 to-green-500">
               Find Your Dream Car with AIxCAR
             </h1>
             <p className="text-xl text-gray-500 mb-8 max-w-2xl mx-auto">
