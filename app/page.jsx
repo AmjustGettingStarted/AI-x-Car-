0a2db7f05f8621a65b0c2f565307b3b607c75151
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-15 15:25:36 +0530

    added the grid to the featured cars section

--- app/page.jsx
+++ app/page.jsx
@@ -31,7 +31,7 @@ export default function Home() {
               View All <ChevronRight className="ml-1 h-4 w-4" />
             </Button>
           </div>
-          <div>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {featuredCars.map((car) => (
               <CarCard key={car.id} car={car} />
             ))}
