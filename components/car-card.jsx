a48fa819254772e7d640349f6f88634b0c84bd22
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-15 15:26:09 +0530

    Added a heart icon for each car, which onClick adds it to wishlist

--- components/car-card.jsx
+++ components/car-card.jsx
@@ -1,24 +1,32 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { Card } from "./ui/card";
 import Image from "next/image";
+import { CarIcon, Heart } from "lucide-react";
+import { Button } from "./ui/button";
 
 const CarCard = ({ car }) => {
+  const [isSaved, setIsSaved] = useState(car.whishListed);
   return (
-    <Card>
-      <div className="relative h-40">
+    <Card className="overflow-hidden hover:shadow-lg transition group">
+      <div className="relative h-48">
         {car.images && car.images.length > 0 ? (
-          <div>
+          <div className="w-full h-full relative">
             <Image
               src={car.images[0]}
               alt={`${car.make} ${car.model}`}
-              width={500}
-              height={500}
+              fill
               className="object-cover group-hover:scale-105 transition-all duration-500"
             />
           </div>
         ) : (
-          <div></div>
+          <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+            <CarIcon className="h-12 w-12 text-gray-400" />
+          </div>
         )}
+        <Button>
+          <Heart className={isSaved ? "fill-current" : ""} size={20} />
+        </Button>
       </div>
     </Card>
   );
