40bfdb224f242fae7bf7aeed523194c5d31f95e0
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-16 15:01:13 +0530

    Initial setup of Search bar/ home search bar compone

--- components/home-search.jsx
+++ components/home-search.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { Input } from "./ui/input";
 
 const HomeSearch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleTextSubmit = () => {};
   return (
-    <div>HomeSearch</div>
-  )
-}
+    <div>
+      <form onSubmit={handleTextSubmit}>
+        <div>
+          <Input
+            type="text"
+            placeholder="Enter make, model, or use our AI Image Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm "
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 
-export default HomeSearch
+export default HomeSearch;
