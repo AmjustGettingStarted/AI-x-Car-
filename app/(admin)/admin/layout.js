cc7e1e2e1f7e4fd487bfc7910a72f04ca2558c69
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-17 12:52:11 +0530

    admin page initial setup done with admin authentication and server actions to check if the user is admin or not

--- app/(admin)/admin/layout.js
+++ app/(admin)/admin/layout.js
@@ -1,7 +1,19 @@
+import { getAdmin } from "@/actions/admin";
+import Header from "@/components/header";
+import { notFound } from "next/navigation";
 import React from "react";
 
 const AdminLayout = async () => {
-  return <div>AdminLayout</div>;
+  const admin = await getAdmin();
+
+  if (!admin.authorized) {
+    return notFound();
+  }
+  return (
+    <div className="h-full">
+      <Header isAdminPage={true} />
+    </div>
+  );
 };
 
 export default AdminLayout;
---
+++ app/(admin)/admin/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div className="h-screen w-full flex items-center justify-center text-5xl text-black">
+      <h1>AdminPage</h1>
+    </div>
+  );
+};
+
+export default AdminPage;
