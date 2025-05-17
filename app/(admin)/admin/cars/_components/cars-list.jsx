1264438b1ade94aeece0ed0ad46a19bce3dc0cb3
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-17 16:41:20 +0530

    carList component, added button and search area to search for cars that are stored i the db

--- app/(admin)/admin/cars/_components/cars-list.jsx
+++ app/(admin)/admin/cars/_components/cars-list.jsx
@@ -1,12 +1,43 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import React from "react";
+import { Input } from "@/components/ui/input";
+import { Plus, Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
 const CarsList = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <div>
-        <Button className="h-4 w-4">Add Cars</Button>
+    <div className="space-y-4">
+      <div className="flex flex-col sm:flex-row gap-4 items-start sm:items-center justify-between">
+        <Button
+          onClick={() => router.push("/admin/cars/create")}
+          className="flex items-center"
+        >
+          <Plus className="h-4 w-4" /> Add Cars
+        </Button>
+        <form
+          action=""
+          onSubmit={handleSearchSubmit}
+          className="w-full flex sm:w-auto"
+        >
+          <div className="relative flex-1">
+            <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-gray-500 " />
+            <Input
+              type="search"
+              placeholder="Search Cars ..."
+              className=" pl-9 w-full sm:w-100"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+        </form>
       </div>
+      {/* Cars Table */}
     </div>
   );
 };
