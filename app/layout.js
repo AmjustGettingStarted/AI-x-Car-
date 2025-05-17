9956968cfe61bd71867a49b8e68ab4dc9a85d27d
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-17 11:46:29 +0530

    Changed the details of the footer

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-gray-800 text-white py-4">
             <div className="container mx-auto text-center">
-              <p>&copy; 2023 AI x CAR. All rights reserved.</p>
+              <p>&copy; 2025 AI x CAR. All rights reserved.</p>
             </div>
           </footer>
         </body>
