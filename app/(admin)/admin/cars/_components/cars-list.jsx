984898fd7fba7f1ae08cf2805144ff48a167ff16
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-17 16:11:21 +0530

    CarsList component initial setup

---
+++ app/(admin)/admin/cars/_components/cars-list.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const CarsList = () => {
+  return (
+    <div>
+      <div>
+        <Button className="h-4 w-4">Add Cars</Button>
+      </div>
+    </div>
+  );
+};
+
+export default CarsList;
