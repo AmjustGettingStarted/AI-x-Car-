dbbf46b7c33ba0032c1f100809c931d654ca2dee
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-20 17:47:42 +0530

    custom hook to handle api data fetching created and initial setup

---
+++ hooks/use-fetch.jsx
@@ -0,0 +1,11 @@
+const { useState } = require("react");
+
+const useFeatch = (cb) => {
+  cosnt [data,setData] = useState(undefined);
+  const [loading,setLoading] = useState(null);
+  const [error,setError] = useState(null);
+
+  const fn = async() => {
+
+  }
+});
