810d3662573ec03e1f4dd1f714f6248c88569701
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-21 12:43:34 +0530

    auth was not imported, this caused the internal500 error

--- actions/cars.js
+++ actions/cars.js
@@ -1,6 +1,7 @@
 "use server";
 import { db } from "@/lib/prisma";
 import { createClient } from "@/lib/supabase";
+import { auth } from "@clerk/nextjs/server";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { revalidatePath } from "next/cache";
 import { cookies } from "next/headers";
