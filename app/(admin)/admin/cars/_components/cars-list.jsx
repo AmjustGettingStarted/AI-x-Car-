5b7bf1d658fca6261b2edccf04b5ee19748a0258
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-22 17:28:34 +0530

    Successful images rendering inside the table, logic done 💕

--- app/(admin)/admin/cars/_components/cars-list.jsx
+++ app/(admin)/admin/cars/_components/cars-list.jsx
@@ -12,7 +12,8 @@ import {
   TableRow,
 } from "@/components/ui/table";
 import useFetch from "@/hooks/use-fetch";
-import { Loader2, Plus, Search } from "lucide-react";
+import { CarIcon, Loader2, Plus, Search } from "lucide-react";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
@@ -86,19 +87,38 @@ const CarsList = () => {
               <Table>
                 <TableHeader>
                   <TableRow>
-                    <TableHead className="w-[100px]">Invoice</TableHead>
+                    <TableHead className="w-12"></TableHead>
+                    <TableHead>Make & Model</TableHead>
+                    <TableHead>Year</TableHead>
+                    <TableHead>Price</TableHead>
                     <TableHead>Status</TableHead>
-                    <TableHead>Method</TableHead>
-                    <TableHead className="text-right">Amount</TableHead>
+                    <TableHead>Featured</TableHead>
+                    <TableHead className="text-right">Actions</TableHead>
                   </TableRow>
                 </TableHeader>
                 <TableBody>
-                  <TableRow>
-                    <TableCell className="font-medium">INV001</TableCell>
-                    <TableCell>Paid</TableCell>
-                    <TableCell>Credit Card</TableCell>
-                    <TableCell className="text-right">$250.00</TableCell>
-                  </TableRow>
+                  {carsData.data.map((car) => {
+                    return (
+                      <TableRow key={car.id}>
+                        <TableCell className="w-10 h-10 rounded-md overflow-hidden">
+                          {car.images && car.images.length > 0 ? (
+                            <Image
+                              src={car.images[0]}
+                              alt={`${car.make} ${car.model}`}
+                              height={40}
+                              width={40}
+                              className="w-full h-full object-cover"
+                              priority
+                            />
+                          ) : (
+                            <div className="w-full h-full bg-gray-200 flex items-center justify-center">
+                              <CarIcon className="h-6 w-6 text-gray-400" />
+                            </div>
+                          )}
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
                 </TableBody>
               </Table>
             </div>
