9eefc2f59c985813f3ce0525db384cc5dcab4bb6
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-23 12:43:17 +0530

    Tabs section inital setup in Settings Form component

--- app/(admin)/admin/settings/_components/settings-form.jsx
+++ app/(admin)/admin/settings/_components/settings-form.jsx
@@ -1,9 +1,33 @@
 "use client";
 
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Clock, Shield } from "lucide-react";
 import React from "react";
 
 const SettingsForm = () => {
-  return <div>SettingsForm</div>;
+  return (
+    <div className="space-y-6">
+      <Tabs defaultValue="hours" className="w-[400px]">
+        <TabsList>
+          <TabsTrigger value="hours">
+            <Clock className="w-4 h-4 mr-2" /> Working Hours
+          </TabsTrigger>
+          <TabsTrigger value="admins">
+            <Shield className="w-4 h-4 mr-2" /> Admin Users
+          </TabsTrigger>
+        </TabsList>
+        <TabsContent value="hours" className="space-y-6 mt-6">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="account">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="admins" className="space-y-6 mt-6">
+          Change your password here.
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 };
 
 export default SettingsForm;
--- app/(admin)/admin/settings/page.jsx
+++ app/(admin)/admin/settings/page.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import SettingsForm from "./_components/settings-form";
 
 export const metadata = {
   title: " Settings 🛠 | Ai x CAR",
@@ -8,7 +9,9 @@ export const metadata = {
 const SettingsPage = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-6">SettingsPage</h1>
+      <h1 className="text-2xl font-bold mb-6">
+        <SettingsForm />
+      </h1>
     </div>
   );
 };
