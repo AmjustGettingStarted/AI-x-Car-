df394704e6d45f8655a3b1dbf9b3ead7fea83c02
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-27 15:05:46 +0530

    Added Scroll Area to Browse by Brans and added new Brands to the collections

--- app/page.jsx
+++ app/page.jsx
@@ -78,27 +78,32 @@ export default async function Home() {
               </Link>
             </Button>
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-            {carMakes.map((make) => {
-              return (
-                <Link
-                  href={`/cars?make=${make.name}`}
-                  key={make.name}
-                  className="bg-white rounded-lg shadow p-4 text-center hover:shadow-md transition"
-                >
-                  <div className="h-16 w-auto mx-auto mb-2 relative">
-                    <Image
-                      src={make.image}
-                      fill
-                      alt={make.image}
-                      className="object-contain"
-                    />
-                  </div>
-                  <h3 className="font-medium">{make.name}</h3>
-                </Link>
-              );
-            })}
-          </div>
+          {/* Scroll Area */}
+          <ScrollArea className="w-full whitespace-nowrap rounded-md border">
+            {/* <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4"> */}
+            <div className="w-full flex space-x-4">
+              {carMakes.map((make) => {
+                return (
+                  <Link
+                    href={`/cars?make=${make.name}`}
+                    key={make.name}
+                    className="bg-white rounded-lg shadow p-4 text-center hover:shadow-md transition"
+                  >
+                    <div className="min-h-16 min-w-[150px] mx-auto mb-2 relative">
+                      <Image
+                        src={make.image}
+                        fill
+                        alt={make.image}
+                        className="object-contain"
+                      />
+                    </div>
+                    <h3 className="font-medium">{make.name}</h3>
+                  </Link>
+                );
+              })}
+            </div>
+            <ScrollBar orientation="horizontal" className={`hidden`} />
+          </ScrollArea>
         </div>
       </section>
 
