157e963ab65dc85bf36549a153b0a9bcc6bc74d2
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-28 11:32:51 +0530

    Test Drive Page initial setup

---
+++ app/(main)/test-drive/[id]/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export async function generateMetadata() {
+  return {
+    title: `Book Test Drive 📑 | AIxCAR`,
+    description: `Schedule a test drive in few seconds`,
+  };
+}
+
+const TestDrivePage = () => {
+  return <div>TestDrivePage</div>;
+};
+
+export default TestDrivePage;
