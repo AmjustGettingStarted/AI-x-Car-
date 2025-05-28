aadc2e4f735a335dbfd2f2c19b1352927700edd7
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-28 11:59:53 +0530

    Test Drive Form initial setup

---
+++ app/(main)/test-drive/[id]/_component/test-drive-form.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TestDriveForm = ({ car, testDriveInfo }) => {
+  return <div>TestDriveForm</div>;
+};
+
+export default TestDriveForm;
--- app/(main)/test-drive/[id]/page.jsx
+++ app/(main)/test-drive/[id]/page.jsx
@@ -1,5 +1,7 @@
 import { getCarById } from "@/actions/car-listings";
+import { notFound } from "next/navigation";
 import React from "react";
+import TestDriveForm from "./_component/test-drive-form";
 
 export async function generateMetadata() {
   return {
@@ -17,7 +19,15 @@ const TestDrivePage = async ({ params }) => {
   if (!result.success) {
     notFound();
   }
-  return <div>TestDrivePage: {id}</div>;
+  return (
+    <div className="container mx-auto px-4 py-12">
+      <h1 className="text-6xl mb-6 gradient-title">Book a Test Drive</h1>
+      <TestDriveForm
+        car={result.data}
+        testDriveInfo={result.data.testDriveInfo}
+      />
+    </div>
+  );
 };
 
 export default TestDrivePage;
