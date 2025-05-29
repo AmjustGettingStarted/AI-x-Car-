294181af88e61c9f4b68db5af526d6325ad39f9a
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-05-29 11:49:34 +0530

    Meta deta & user authentication logic + test drive data from db logic added

--- app/(main)/reservations/page.jsx
+++ app/(main)/reservations/page.jsx
@@ -1,6 +1,23 @@
+import { getUserTestDrives } from "@/actions/test-drive";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
-const ReservationsPage = () => {
+export const metadata = {
+  title: "My Reservations 🗃 | AIxCAR",
+  description: "Manage your test drive reservations",
+};
+
+
+const ReservationsPage = async () => {
+    // Check user authentication on server
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in?redirect=/reservations");
+  }
+
+  // Fetch reservations on the server
+  const reservationsResult = await getUserTestDrives();
+
   return <div>ReservationsPage</div>;
 };
 
