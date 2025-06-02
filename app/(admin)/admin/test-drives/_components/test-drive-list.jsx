65c1e79cf6afabd01f529da612a11fe67c1e0cd7
AmJustGettingStarted <gamerhmv2050@gmail.com>
2025-06-02 17:29:49 +0530

    feat(Admin Test Drive page): Add new server functions

--- app/(admin)/admin/test-drives/_components/test-drive-list.jsx
+++ app/(admin)/admin/test-drives/_components/test-drive-list.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client";
+import { getAdminTestDrives, updateTestDriveStatus } from "@/actions/admin";
+import { cancelTestDrive } from "@/actions/test-drive";
+import useFetch from "@/hooks/use-fetch";
+import React, { useState } from "react";
 
 const TestDriveList = () => {
-  return (
-    <div>TestDriveList</div>
-  )
-}
+  const [search, setSearch] = useState("");
+  const [statusFilter, setStatusFilter] = useState("");
 
-export default TestDriveList
+  // Custom hooks for API calls
+  const {
+    loading: fetchingTestDrives,
+    fn: fetchTestDrives,
+    data: testDrivesData,
+    error: testDrivesError,
+  } = useFetch(getAdminTestDrives);
+
+  const {
+    loading: updatingStatus,
+    fn: updateStatusFn,
+    data: updateResult,
+    error: updateError,
+  } = useFetch(updateTestDriveStatus);
+
+  const {
+    loading: cancelling,
+    fn: cancelTestDriveFn,
+    data: cancelResult,
+    error: cancelError,
+  } = useFetch(cancelTestDrive);
+  return <div>TestDriveList</div>;
+};
+
+export default TestDriveList;
