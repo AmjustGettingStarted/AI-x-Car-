cea929ba3536e9b18fb8e67de4e678aa45d3c48c
AmjustGettingStarted <gamerhmv2050@gmail.com>
2025-12-07 20:48:00 +0530

    feat(home search) enhance search button with new ui

--- components/home-search.jsx
+++ components/home-search.jsx
@@ -129,7 +129,7 @@ const HomeSearch = () => {
             onChange={(e) => setSearchTerm(e.target.value)}
             className="pl-10 pr-12 py-6 w-full rounded-full border-gray-300 bg-white/95 backdrop-blur-sm  sm:hidden"
           />
-          <div className="absolute right-[100px]">
+          <div className="absolute right-[100px] hover:bg-black rounded-full hover:text-white">
             <Camera
               size={35}
               onClick={() => setImageSearchActive(!imageSearchActive)}
